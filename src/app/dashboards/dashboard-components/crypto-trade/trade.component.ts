04c1872e7b30c823a6f950627d052b5db8b39c7c
Guilherme Melo <guilhermeromelo@gmail.com>
2022-02-17 21:01:04 -0300

    feat: Template

---
+++ src/app/dashboards/dashboard-components/crypto-trade/trade.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-trade',
+  templateUrl: './trade.component.html'
+})
+export class TradeComponent {
+  active = 1;
+  constructor() {}
+}
